2804689e2bd0e5a7609dca825779281beaf8ab51
LeDucViet <ldviettb04@gmail.com>
2025-12-05 11:34:01 +0700

    first commit

---
+++ src/test/java/com/example/bookingmeeting_be/BookingmeetingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingmeeting_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingmeetingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
