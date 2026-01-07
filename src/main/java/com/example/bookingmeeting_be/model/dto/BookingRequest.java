94424947e33b649fa0349b0b56d01b218f6a50d2
DucHai <haidao2004bt@gmail.com>
2026-01-07 18:08:01 +0700

    add BookingRequest in model/dto

---
+++ src/main/java/com/example/bookingmeeting_be/model/dto/BookingRequest.java
@@ -0,0 +1,22 @@
+package com.example.bookingmeeting_be.model.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class BookingRequest {
+    private String title;
+    private Integer roomId;
+    private Integer hostUserId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private List<DeviceRequest> devices;
+
+    @Data
+    public static class DeviceRequest {
+        private Integer deviceId;
+        private int quantity;
+    }
+}
